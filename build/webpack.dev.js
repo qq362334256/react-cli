f09307506e58fd96d21fc2910bcb8aec607e13df
miaoyu <362334256@qq.com>
2017-04-14 01:02:52 +0800

    初始版本

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,15 +1,84 @@
-const { module: {rules} } = require('./webpack.base.js');
+const 
+	path = require('path'),
+	webpack = require('webpack'),
+	dirName = path.resolve(__dirname, './../'),
+	{ devServer: {port}, devConfig: {RESOURCES_URL: publicPath} } = require('./config.json'),
+	{ context, entry, output, crossOriginLoading, resolve, plugins, module: {rules} } = require('./webpack.base.js');
+
 
 // 导出开发配置对象
 module.exports = {
+	context,
+	entry,
+	resolve,
+	output: {
+		publicPath,
+		crossOriginLoading,
+		path: output.path,
+		// 打包文件命名规则
+		filename: 'js/[name].js',
+		// 异步按需加载文件命名规则
+		chunkFilename: 'js/[name].js',
+	},
 	module: {
 		rules: [...rules, {
 			test: /\.less$/,
-			use: {
+			use: [
 				'style-loader',
-				{ loader: 'css-loader', options: { importLoaders: 1 } },
+				{ 
+					loader: 'css-loader', 
+					options: { importLoaders: 1 } 
+				},
           		'less-loader'
-			}
+			]
 		}]
+	},
+  	plugins: [
+  		...plugins,
+	    // 设置nodejs环境常量
+		new webpack.DefinePlugin({
+			// 设置为开发环境
+      		'process.env.NODE_ENV': JSON.stringify('development')
+    	}),
+    	// 测试服务自动hot插件
+    	new webpack.HotModuleReplacementPlugin()
+	],
+	// 测试服务
+	devServer: {
+		// 请求端口
+		port,
+		// ip地址
+		host: '127.0.0.1',
+		// 控制台和devtool显示消息
+		clientLogLevel: 'error',
+		// 所有的服务都启动gzip压缩
+		compress: true,
+		// 指定测试服务的资源目录
+		contentBase: path.join(dirName, 'dist'),
+		// 请求404的时候跳转到index.html页面
+		historyApiFallback: true,
+		// 添加热替换功能
+		hot: true,
+		// 防止热替换失败
+		hotOnly: true,
+		// 禁止惰性模式
+		lazy: false,
+		// 隐藏无用信息
+		noInfo: true,
+		// 控制台不答应任何信息，包括错误
+		// quiet: false,
+		// 展示精确的bundel信息展示,使用 quiet 或 noInfo 时，此选项无效
+		// stats: 'errors-only',
+	},
+	// 资源映射
+	devtool: 'cheap-module-source-map',
+	// 监听文件变化
+	watch: true,
+	// 监听文件变化配置
+	watchOptions: {
+		// 排除大文件监听
+		ignored: /node_modules/,
+		// 监听失效轮询时间
+		poll: 1000
 	}
 };
--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -1,9 +1,22 @@
 const 
+	webpack = require('webpack'),
 	extractTextPlugin = require('extract-text-webpack-plugin'),
-	{ module: {rules} } = require('./webpack.base.js');
+	{ maxFileSize, proConfig: {RESOURCES_URL: publicPath} } = require('./config.json'),
+	{ context, entry, crossOriginLoading, resolve, plugins, output: {path}, module: {rules} } = require('./webpack.base.js');
 
 // 导出生产配置对象
 module.exports = {
+	context,
+	entry,
+	resolve,
+	output: {
+		path,
+		publicPath,
+		// 打包文件命名规则
+		filename: 'js/[name].[chunkhash].min.js',
+		// 异步按需加载文件命名规则
+		chunkFilename: 'js/[name].[chunkhash].min.js',
+	},
 	module: {
 		rules: [...rules, {
 			test: /\.less$/,
@@ -14,13 +27,30 @@ module.exports = {
 	              	options: { importLoaders: 1 }
 	            }, {
 	              	loader: 'less-loader',
-	            }
+	            },
+	            publicPath: '../'
 	          ]
 	        )
 		}]
 	},
   	plugins: [
+  		...plugins,
 	    // 提取 CSS 到单独的文件中
-	    new ExtractTextPlugin('[name].[chunkHash].css')
-	]
+	    new ExtractTextPlugin('css/[name].[chunkhash].min.css'),
+	    // 压缩文件
+	    new webpack.optimize.UglifyJsPlugin(),
+	    // 设置nodejs环境常量
+		new webpack.DefinePlugin({
+			// 设置为开发环境
+      		'process.env.NODE_ENV': JSON.stringify('production')
+    	})
+	],
+	performance: {
+		// 打包文件超过默认值就提示
+  		hints: 'warning',
+  		// 打包入口最大文件大小，超过则会在控制台提示
+  		maxEntrypointSize: maxFileSize，
+  		// 打包出口最大文件大小，超过则会在控制台提示
+  		maxAssetSize: maxFileSize
+	}
 };
